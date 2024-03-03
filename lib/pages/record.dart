65f188dbff1dfeead5e59dfc8e138c56601144c2
Gabriel Rodríguez <rgabriel@mit.edu>
2024-03-03 13:10:51 -0500

    more packages which may be needed

--- lib/pages/record.dart
+++ lib/pages/record.dart
@@ -7,4 +7,4 @@ class RecordPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Placeholder();
   }
-} 
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
+import just_audio
+import just_waveform
+import mic_stream
+import path_provider_foundation
 import record_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  JustWaveformPlugin.register(with: registry.registrar(forPlugin: "JustWaveformPlugin"))
+  MicStreamPlugin.register(with: registry.registrar(forPlugin: "MicStreamPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
 }
